d1e6bbd701b19251085de673f15c3dfe1244a2bb
diges <dmitrymobtech@gmail.com>
2014-02-16 01:34:38 -0800

    bug fixed

--- Week06-NodeModules-Dmitry/server.js
+++ Week06-NodeModules-Dmitry/server.js
@@ -2,17 +2,17 @@
  * @author Dmitry
  */
 
-var basicMath = require('Library/basicMath');
-var feetToMiiles = require('Library/feetToMiiles');
-var square = require('square');
+var MathMod = require('./Library/basicMath');
+var feetMod = require('./Public/feetToMilles');
+var squareMod = require('./square');
 var varA=10;
 var varB=20;
 
-console.log("basicMath.add returns: " + basicMath.add(varA,varB));
-console.log("basicMath.substract returns: " + basicMath.substract(varA,varB));
-console.log("basicMath.myltiplay returns: " + basicMath.myltiplay(varA,varB));
-console.log("feetToMiiles returns:"+feetToMiiles.feetToMiiles(varA*varB)); // multiplication just to  make the number larger :)
-console.log("square. returns: " + square.square(varA));
+console.log("basicMath.add returns: " + MathMod.myMath.add(varA,varB));
+console.log("basicMath.substract returns: " + MathMod.myMath.substract(varA,varB));
+console.log("basicMath.myltiplay returns: " + MathMod.myMath.myltiplay(varA,varB));
+console.log("feetToMiiles returns:"+ feetMod.myConverter.feetToMiiles(varA*varB) + " Miles in "+(varA*varB)+" foot :)"); 
+console.log("square returns: " + squareMod.mySquare.square(varA));
 
 
 
